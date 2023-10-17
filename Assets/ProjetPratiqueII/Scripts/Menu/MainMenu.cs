2ce2e029bd7bb6f1bb3cfa8cc6b1a9dd03215a96
Guillaume Picard <guillaumepicard99@gmail.com>
2023-10-17 18:22:17 -0400

    loading screen

--- Assets/ProjetPratiqueII/Scripts/Menu/MainMenu.cs
+++ Assets/ProjetPratiqueII/Scripts/Menu/MainMenu.cs
@@ -2,12 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Slider m_ProgressBar;
+    [SerializeField] private GameObject m_LoadScreen;
+
+    private void Start()
+    {
+        m_LoadScreen.SetActive(false);
+    }
     public void StartNewGame()
     {
-        SceneManager.LoadScene("Game");
+        StartCoroutine(LoadGame());
+    }
+
+    private IEnumerator LoadGame()
+    {
+        AsyncOperation scene = SceneManager.LoadSceneAsync("Game");
+        m_LoadScreen.SetActive(true);
+        while (true)
+        {
+            if (scene.isDone)
+            {
+                SceneManager.UnloadSceneAsync("MainMenu");
+            }
+            else
+            {
+                m_ProgressBar.value = scene.progress;
+                yield return null;
+            }
+        }
     }
 
     public void QuitGame()
